92f46081c948417381a546a075f42b7120e0471d
Vinícius Fumagalli <vinifumagalli_@hotmail.com>
2025-07-16 21:25:57 -0300

    feat(Pagamentos): adiciona migrations

---
+++ src/services/NSE.Pagamentos.Api/Migrations/20250717002452_Initial.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NSE.Pagamentos.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pagamentos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PedidoId = table.Column<int>(type: "int", nullable: false),
+                    TipoPagamento = table.Column<int>(type: "int", nullable: false),
+                    Valor = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pagamentos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Transacoes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CodigoAutorizacao = table.Column<string>(type: "varchar(100)", nullable: false),
+                    BandeiraCartao = table.Column<string>(type: "varchar(100)", nullable: false),
+                    DataTransacao = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ValorTotal = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    CustoTransacao = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    TID = table.Column<string>(type: "varchar(100)", nullable: false),
+                    NSU = table.Column<string>(type: "varchar(100)", nullable: false),
+                    PagamentoId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transacoes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Transacoes_Pagamentos_PagamentoId",
+                        column: x => x.PagamentoId,
+                        principalTable: "Pagamentos",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transacoes_PagamentoId",
+                table: "Transacoes",
+                column: "PagamentoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Transacoes");
+
+            migrationBuilder.DropTable(
+                name: "Pagamentos");
+        }
+    }
+}
